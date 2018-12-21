300f3ea4fb9019b567f430bf87db3dad315569b4
Prashant Pratik <prashant.pratik@cognizant.com>
2018-12-21 19:04:04 +1100

    Updated test cases

--- src/test/java/com/pp/robot/factory/CommandFactoryTest.java
+++ src/test/java/com/pp/robot/factory/CommandFactoryTest.java
@@ -1,30 +1,30 @@
-//package com.anz.robot.factory;
-//
-//import BaseCommand;
-//import com.anz.robot.commands.PlaceCommand;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertTrue;
-//
-//public class CommandFactoryTest {
-//
-//    @Test
-//    public void testPlaceCommand() {
-//        assertTrue(CommandFactory.getCommandInstance("PLACE") instanceof BaseCommand);
-//    }
-//
-//    @Test
-//    public void testLeftCommand() {
-//        assertTrue(CommandFactory.getCommandInstance("LEFT") instanceof BaseCommand);
-//    }
-//
-//    @Test
-//    public void testRightCommand() {
-//        assertTrue(CommandFactory.getCommandInstance("RIGHT") instanceof BaseCommand);
-//    }
-//
-//    @Test
-//    public void testMoveCommand() {
-//        assertTrue(CommandFactory.getCommandInstance("MOVE") instanceof BaseCommand);
-//    }
-//}
+package com.pp.robot.factory;
+
+import com.pp.robot.commands.BaseCommand;
+import com.pp.robot.commands.Command;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CommandFactoryTest {
+
+    @Test
+    public void testPlaceCommand() {
+        assertTrue(CommandFactory.getCommandInstance(new Command("PLACE")) instanceof BaseCommand);
+    }
+
+    @Test
+    public void testLeftCommand() {
+        assertTrue(CommandFactory.getCommandInstance(new Command("LEFT")) instanceof BaseCommand);
+    }
+
+    @Test
+    public void testRightCommand() {
+        assertTrue(CommandFactory.getCommandInstance(new Command("RIGHT")) instanceof BaseCommand);
+    }
+
+    @Test
+    public void testMoveCommand() {
+        assertTrue(CommandFactory.getCommandInstance(new Command("MOVE")) instanceof BaseCommand);
+    }
+}
